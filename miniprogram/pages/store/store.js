94da7bbc61a87d5548cb1b619229a4214514885c
lujiaxing <18620228979@163.com>
2026-05-25 17:05:07 +0800

    feat: 添加门店信息页

---
+++ miniprogram/pages/store/store.js
@@ -0,0 +1,28 @@
+const store = require("../../config/store");
+const contact = require("../../utils/contact");
+
+Page({
+  data: {
+    store,
+    faqs: [
+      "支持到店看实物，花色和尺寸以门店现货为准。",
+      "普通商品可微信咨询，特价商品建议尽快到店确认。",
+      "第一版小程序不支持线上支付，下单和付款请联系门店。"
+    ]
+  },
+  onShareAppMessage() {
+    return {
+      title: store.shareTitle,
+      path: "/pages/store/store"
+    };
+  },
+  handleWechat() {
+    contact.copyWechat();
+  },
+  handlePhone() {
+    contact.callStore();
+  },
+  handleLocation() {
+    contact.openStoreLocation();
+  }
+});
