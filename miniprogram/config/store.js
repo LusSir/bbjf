38f35248a0642198148d049c09cdc6d3c776333a
lujiaxing <18620228979@163.com>
2026-05-26 10:08:37 +0800

    chore: 更新门店联系信息

--- miniprogram/config/store.js
+++ miniprogram/config/store.js
@@ -1,10 +1,10 @@
 const store = {
   name: "贝贝家纺",
   slogan: "实体门店 · 成品床品 · 到店可看实物",
-  phone: "",
+  phone: "18585204552",
   wechatId: "",
-  address: "请补充门店完整地址",
-  businessHours: "请补充营业时间",
+  address: "贵州省遵义市红花岗区内环路",
+  businessHours: "8:00-18:00",
   latitude: null,
   longitude: null,
   shareTitle: "贝贝家纺成品床品，到店可看实物"
